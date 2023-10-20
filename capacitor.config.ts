99063e4da35d7d4844ae3bb92b00a539de66f1f6
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-20 21:14:11 +0100

    changed bundler

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'micusalat',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+	appId: 'icu.mosque.salat',
+	appName: 'micusalat',
+	webDir: 'dist',
+	server: {
+		androidScheme: 'https',
+	},
 };
 
 export default config;
