8d038f625113b359e16734847b73186b78810d3c
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-20 21:37:07 +0100

    build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'icu.mosque.salat',
+	appId: 'com.icumosque.salat ',
 	appName: 'micusalat',
 	webDir: 'dist',
 	server: {
