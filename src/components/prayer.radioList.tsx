999e509c7c485b276bfd9213d6227f5a1f56638c
hossam sulleman <93336567+hossam1231@users.noreply.github.com>
2023-10-21 23:42:48 +0100

    fix type errors

--- src/components/prayer.radioList.tsx
+++ src/components/prayer.radioList.tsx
@@ -1,113 +1,3 @@
-// import React, { useState } from 'react';
-// import { CheckCircleIcon } from '@heroicons/react/outline'; // Use the outline version for the check icon
-// import {
-// 	Card,
-// 	Title,
-// } from '@tremor/react';
-
-// const mailingLists = [
-// 	{
-// 		id: 1,
-// 		title: 'Fajir',
-// 		description:
-// 			'Prayer starts at 12:42',
-// 		users: '621 users',
-// 	},
-// 	{
-// 		id: 2,
-// 		title: 'Duhur',
-// 		description:
-// 			'Prayer starts at 12:42',
-// 		users: '1200 users',
-// 	},
-// 	{
-// 		id: 3,
-// 		title: 'Duhur',
-// 		description:
-// 			'Prayer starts at 12:42',
-// 		users: '1200 users',
-// 	},
-// 	{
-// 		id: 2,
-// 		title: 'Duhur',
-// 		description:
-// 			'Prayer starts at 12:42',
-// 		users: '1200 users',
-// 	},
-// 	{
-// 		id: 2,
-// 		title: 'Duhur',
-// 		description:
-// 			'Prayer starts at 12:42',
-// 		users: '1200 users',
-// 	},
-// ];
-
-// export default function PrayerRadio() {
-// 	const [
-// 		selectedMailingList,
-// 		setSelectedMailingList,
-// 	] = useState(null);
-
-// 	const handleSelect = (id) => {
-// 		setSelectedMailingList(id);
-// 	};
-
-// 	return (
-// 		<div className='mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-x-4'>
-// 			{mailingLists.map(
-// 				(mailingList) => (
-// 					<Card
-// 						key={mailingList.id}
-// 						className={`relative flex cursor-pointer rounded-lg border ${
-// 							selectedMailingList ===
-// 							mailingList.id
-// 								? 'border-blue-600 ring-2 ring-blue-600'
-// 								: 'border-[#1f2937]'
-// 						} bg-[#0f131e] p-4 shadow-sm focus:outline-none`}
-// 						onClick={() =>
-// 							handleSelect(
-// 								mailingList.id
-// 							)
-// 						}
-// 					>
-// 						<div className='flex flex-1'>
-// 							<div className='flex flex-col'>
-// 								<span className='block text-sm font-medium text-gray-200'>
-// 									{
-// 										mailingList.title
-// 									}
-// 								</span>
-// 								<span className='mt-1 flex items-center text-sm text-gray-500'>
-// 									{
-// 										mailingList.description
-// 									}
-// 								</span>
-// 							</div>
-// 						</div>
-// 						{selectedMailingList ===
-// 							mailingList.id && (
-// 							<CheckCircleIcon
-// 								className='h-5 w-5 text-blue-600'
-// 								aria-hidden='true'
-// 							/>
-// 						)}
-// 						<span
-// 							className={`border ${
-// 								selectedMailingList ===
-// 								mailingList.id
-// 									? 'border-blue-600'
-// 									: 'border-2'
-// 							} pointer-events-none absolute -inset-px rounded-lg`}
-// 							aria-hidden='true'
-// 						/>
-// 					</Card>
-// 				)
-// 			)}
-// 		</div>
-// 	);
-// }
-
 import React, { useState } from 'react';
 import { CheckIcon } from '@heroicons/react/outline';
 import {
@@ -152,9 +42,11 @@ export default function PrayerRadio() {
 	const [
 		selectedPrayers,
 		setSelectedPrayers,
-	] = useState([]);
+	] = useState<number[]>([]); // Specify the type as number[] for selectedPrayers
 
-	const handleSelect = (id) => {
+	const handleSelect = (
+		id: number
+	) => {
 		if (
 			selectedPrayers.includes(id)
 		) {
@@ -183,7 +75,7 @@ export default function PrayerRadio() {
 						)
 							? 'border-blue-600 ring-2 ring-blue-600'
 							: 'border-[#1f2937]'
-					} bg-[#0f131e] p-4 shadow-sm focus:outline-none`}
+					}  p-4 shadow-sm focus:outline-none`}
 					onClick={() =>
 						handleSelect(
 							prayer.id
